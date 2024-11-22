9c015e3f2a82bba8ba20828efb71e0801de0a389
arghaahit <argha.ahit@gmail.com>
2024-11-22 11:13:10 +0530

    changs

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:musicproject/login/view/login.page.dart';
 import 'package:musicproject/perticuler/service/song.controller.dart';
 import 'package:musicproject/selectSinger/views/selectsinger.dart';
 import 'package:musicproject/splash/splash.screen.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,8 +24,40 @@ Future<void> main() async {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Future<void> _requestPermissions() async {
+    // Request Internet permissions (by default handled by manifest)
+    
+
+    // Request Microphone permissions
+    PermissionStatus micStatus = await Permission.microphone.request();
+    if (micStatus.isGranted) {
+      print('Microphone permission granted');
+    } else {
+      print('Microphone permission denied');
+    }
+
+    // Request Audio Recording permissions
+    PermissionStatus recordStatus = await Permission.microphone.request();
+    if (recordStatus.isGranted) {
+      print('Audio recording permission granted');
+    } else {
+      print('Audio recording permission denied');
+    }
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _requestPermissions();
+  }
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
